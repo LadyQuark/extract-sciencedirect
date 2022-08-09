438f9effef7074378a0c03afbe97f4e84c9a4298
Dipika <dips@Dipikas-iMac.local>
2022-08-09 10:12:24 +0530

    Added all files

---
+++ articles_scd.py
@@ -0,0 +1,156 @@
+import json
+import os
+import re
+import requests
+from sys import argv, exit
+from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+
+from transform_for_db import transform_sc_item
+
+# Get API key from .env
+load_dotenv(find_dotenv())
+API_KEY = os.getenv('API_KEY')
+
+def main():
+    # Get file containing list of links
+    filename = argv[1] if len(argv) == 2 else "links.txt"
+
+    # Extract PIIs from links
+    try:
+        id_list =  extract_pub_ids(filename)
+    except Exception as e:
+        print(e)
+        exit(1)
+
+    # Initialize variables
+    failed = {}
+    articles = []
+
+    # Loop though every publication ID in list
+    for id in id_list:
+        
+        # Construct request URL
+        url = f"https://api.elsevier.com/content/article/pii/{id}?apiKey={API_KEY}&httpAccept=application%2Fjson"
+        # Get response
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        # Handle errors
+        except requests.RequestException as e:
+            match response.status_code:
+                case 400:
+                    # If invalid PII, log & move to next podcast
+                    print(f"Invalid PII/publication ID {id}")
+                    failed[id] = f"Invalid PII/publication ID"                
+                    continue
+                case 404:
+                    # If resource not founf, log & move to next podcast
+                    print(f"Resource not found for {id}")
+                    failed[id] = f"Resource not found"                
+                    continue
+                case _:
+                    # For all else, especially authorization error/Quota exceeded
+                    print(f"Unable to fetch article {id}: {e}")
+                    failed[id] = f"Unable to fetch: {e}"
+                    exit(1)
+        # Get JSON response
+        data = response.json()
+        # Check key
+        if 'full-text-retrieval-response' not in data:
+            print(f"Unable to parse article {id}")
+            failed[id] = f"Unable to parse article {id}"
+            continue
+        data_dict = data['full-text-retrieval-response']
+
+        # Transform into desired format
+        try:
+            article = transform_sc_item(data_dict)
+        except Exception as e:
+            print(f"Failed to transform data for {id}. See failed_article.json")
+            failed[id] = f"Failed to transform data"
+        else:
+            articles.append(article)
+
+    # Create json file for transformed episodes in folder `ki_json`
+    create_json_file(
+        folder="ki_json", name="articles.json", source_dict=articles, failed=failed
+    )
+
+    # Create json file for errors if any
+    if len(failed) > 0:
+        create_json_file(
+            folder="", name="failed", source_dict=failed, failed=failed
+        )
+
+
+def extract_pub_ids(filename):
+    id_list = []
+    
+    # Make path
+    try:
+        filepath = os.path.join(filename)
+    except Exception as e:
+        raise Exception(e)
+
+    # Check path exists
+    if not os.path.exists(filepath):
+        raise Exception(f"Could not find file {filepath}")
+
+    # Open file and get list of lines
+    with open(filepath, 'r') as f:
+        lines = [line.strip() for line in f.readlines()]
+    
+    # Extract all PII in urls
+    for line in lines:
+        fragments = line.split('pii/')
+        if len(fragments) == 2:
+            id_list.append(fragments[1])
+
+    return id_list
+
+
+def create_json_file(folder, name, source_dict, failed):
+    """ 
+    Create json file from dict and save to given folder and filename 
+    and log errors in dict `failed`
+    """
+    
+    # Convert `data_dict` to JSON formatted string
+    json_string = json.dumps(source_dict, indent=4)
+    try:
+        # Create valid file name
+        filename = f"{get_valid_filename(name)}.json"
+        # Create folder if does not already exist
+        Path(folder).mkdir(parents=True, exist_ok=True)
+        # Join folder and filename
+        filepath = os.path.join(folder, filename)
+    except Exception as e:
+        print(e)
+        failed[name] = "Could not create valid file name or folder"
+        return
+    # Write to JSON file
+    with open(filepath, 'w') as file:
+            file.write(json_string)
+
+
+def get_valid_filename(name):
+    """
+    modified from: https://github.com/django/django/blob/main/django/utils/text.py
+
+    Return the given string converted to a string that can be used for a clean
+    filename. Remove leading and trailing spaces; convert other spaces to
+    underscores; and remove anything that is not an alphanumeric, dash,
+    underscore, or dot.
+    >>> get_valid_filename("john's portrait in 2004.jpg")
+    'johns_portrait_in_2004.jpg'
+    """
+    s = str(name).strip().replace(" ", "_")
+    s = re.sub(r"(?u)[^-\w.]", "", s)
+    if s in {"", ".", ".."}:
+        raise Exception("Could not derive file name from '%s'" % name)
+    return s
+
+
+if __name__ == "__main__":
+    main()
---
+++ transform_for_db.py
@@ -0,0 +1,104 @@
+from datetime import datetime
+from dateutil.parser import parse
+import json
+
+DEFAULT_VALUES = {
+    'thumbnail': None,
+    'permission': "Global",
+    'mediaType': "article",
+    'tags': "research",
+    'type': "ki",
+    'transcript': "",
+    'createdBy': None,
+    'updated': "",
+    'isDeleted': False,
+}
+
+def main():
+    with open("test.json", "r") as file:
+        data = json.load(file)
+    
+    article = transform_sc_item(data)
+    with open('ki_test.json', 'w', encoding='utf-8') as f:
+        json.dump(article, f, ensure_ascii=False, indent=4)
+
+
+def standard_date(pub_date):
+    if pub_date:
+        try:
+            date = parse(pub_date)
+            pub_date = date.strftime("%Y-%m-%d")
+        except ValueError:
+            return None
+    
+    return pub_date
+
+
+def standard_duration(audio_length):
+    if audio_length:
+        try:
+            time = datetime.strptime(audio_length, "%H:%M:%S")
+        except ValueError:
+            try:
+                time = datetime.strptime(audio_length, "%M:%S")
+            except ValueError:
+                return None
+
+        audio_length = time.strftime("%H:%M:%S")
+    
+    return audio_length
+    
+
+def timestamp_ms():
+    utc_time = datetime.utcnow()
+    return int(utc_time.timestamp() * 1000)
+
+
+def transform_sc_item(article):
+    coredata = article['coredata']    
+    
+    url = None
+    for link in coredata.get("link", []):
+        if link['@rel'] == "scidir":
+            url = link['@href']
+
+
+    try:
+        db_item = {
+            'title': coredata.get('dc:title'), 
+            'thumbnail': DEFAULT_VALUES['thumbnail'], 
+            'description': coredata.get('dc:description'), 
+            'permission': DEFAULT_VALUES['permission'], 
+            'authors': [creator["$"] for creator in coredata.get('dc:creator', [])],
+            'mediaType': DEFAULT_VALUES['mediaType'], 
+            'tags': DEFAULT_VALUES['tags'], 
+            'type': DEFAULT_VALUES['type'], 
+            'metadata': {
+                'url': url,
+                'transcript': article.get('originalText', DEFAULT_VALUES['transcript']),
+                }, 
+            'created': {
+                '$date': {
+                    '$numberLong': str(timestamp_ms())
+                    }
+                }, 
+            'createdBy': DEFAULT_VALUES['createdBy'],
+            'updated': DEFAULT_VALUES['updated'],
+            'isDeleted': DEFAULT_VALUES['isDeleted'],
+            'original': [article], 
+            'publishedDate': standard_date(coredata.get('prism:coverDate'))
+        }
+    except KeyError as e:
+        print(e)
+        # Convert `episode` dict to JSON formatted string
+        json_string = json.dumps(article, indent=4)
+        # Write to JSON file
+        with open('failed_article.json', 'w') as file:
+            file.write(json_string)
+        raise Exception("Key Error")
+
+    return db_item
+
+
+if __name__ == "__main__":
+    main()
